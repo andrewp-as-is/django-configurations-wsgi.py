e161b88da42439e4d6272eb3bb665a7a46ad4fab
andrewp-as-is <russianidiot.github@gmail.com>
2020-06-16 14:16:58 +0300

    init

--- django_configurations_wsgi.py
+++ django_configurations_wsgi.py
@@ -1,9 +1,8 @@
 import os
 
-if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-    os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 
-    from configurations.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 
-    application = get_wsgi_application()
+application = get_wsgi_application()
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-configurations-wsgi',
-    version='2020.6.12',
+    version='2020.6.13',
     install_requires=[
         'django-configurations',
         'setuptools',

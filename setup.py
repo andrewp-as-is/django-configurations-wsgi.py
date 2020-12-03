2209167d480273ac1a67541515dfb44c44d1b75d
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-03 22:57:43 +0300

    2020:12:03 22:57

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='django-configurations-wsgi',
+    version='2020.7.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages()
 )
